7b689dc3cab0dd30fa219d038ec91f709e0b69ee
saurabh919yadav <saurabh.yadav919@gmail.com>
2020-07-08 23:21:27 +0530

    Create template file for cpp

---
+++ skeleton.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define F first
+#define S second
+#define PB push_back
+
+typedef vector<int> vi;
+typedef long long ll;
+
+int main(){
+	ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+	
+}
